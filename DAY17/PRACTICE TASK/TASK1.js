a368c33c7246ca532c431b8837aee9a6fa0e6fa6
vignesh2526 <vigneshb2567@gmail.com>
2025-12-09 10:33:20 +0530

    ASSIGNMENT

---
+++ DAY17/PRACTICE TASK/TASK1.js
@@ -0,0 +1,12 @@
+// declaring using variables
+
+var name="Vicky";
+var age="19"
+console.log(name,age);
+
+let now="student";
+let collage="M.G.R University";
+console.log(now,collage)
+
+const from="KODUNGAIYUR";
+console.log(from);
