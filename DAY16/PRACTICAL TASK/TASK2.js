3613d63f8e9e6f7c95d9597fa199460060a1663b
vignesh2526 <vigneshb2567@gmail.com>
2025-12-20 13:34:10 +0530

    ASSIGNMENT

---
+++ DAY16/PRACTICAL TASK/TASK2.js
@@ -0,0 +1,57 @@
+let details =[
+     {
+  name: "Vicky",
+  age: 19,
+  course: "Full stack/python",
+  country: "India",
+  goal: "Become a web developer"
+},
+{
+    name:"sathish",
+    age: 20,
+    course: "Full stack/python",
+    country: "India",
+    goal: "Become a web developer "
+},
+{
+    name:"BALA",
+    age: 20,
+    course: "Full stack/JAVA",
+    country: "India",
+    goal: "Become a web developer "
+},
+{
+    name:"vigneshwaran",
+    age: 19,
+    course: "Full stack/Python",
+    country: "India",
+    goal: "Become a web developer "
+},
+{
+    name:"vigneshwaran(JHONNY)",
+    age: 20,
+    course: "Full stack/python",
+    country: "India",
+    goal: "Become a web developer "
+},
+{
+    name:"ALI",
+    age: 20,
+    course: "Full stack/JAVA",
+    country: "India",
+    goal: "Become a web developer "
+},
+{
+    name:"AJAY SS",
+    age: 20,
+    course: "Full stack/JAVA",
+    country: "India",
+    goal: "Become a web developer "
+}
+];
+
+
+console.log(details);
+
+
+
---
+++ DAY16/PRACTICAL TASK/TASK4.js
@@ -0,0 +1,9 @@
+// to write with explaination comments
+
+let a="vicky";// stored a string in a variable named a
+let b=19;// stored a integer in a variable named b
+let arr=[[1,2,3,[4,5,6]],[77,88,99]];//stored a multi dimensional array in arr
+
+console.log(a);// to print value in a variable
+console.log(b);// to print value in b variable
+console.log(arr);// to print value in arr variable
